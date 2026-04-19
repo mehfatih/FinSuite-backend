d6bf983cb951be38dc3b3f3fee1b3ae0e682ec2b
mehfatih <meh.fatih77@gmail.com>
2026-04-19 08:38:21 +0300

    fix: efatura stub

--- src/routes/efatura.ts
+++ src/routes/efatura.ts
@@ -1,14 +1,4 @@
 import { Router } from "express";
-import { eFaturaController } from "../controllers/eFaturaController";
-import { merchantAuth } from "../middleware/auth";
-
 const router = Router();
-
-router.get("/stats", merchantAuth, eFaturaController.stats);
-router.get("/", merchantAuth, eFaturaController.list);
-router.get("/:id", merchantAuth, eFaturaController.getById);
-router.get("/:id/xml", merchantAuth, eFaturaController.downloadXML);
-router.post("/", merchantAuth, eFaturaController.create);
-router.post("/:id/cancel", merchantAuth, eFaturaController.cancel);
-
+router.get("/", (_req, res) => res.json({ success: true, data: [] }));
 export default router;
